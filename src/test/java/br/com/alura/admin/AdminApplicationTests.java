b17b968e1548e2f92746371224e716943a00c416
igorcastro-dsn <igorcastro.dsn@gmail.com>
2021-01-03 16:22:12 -0300

    INIT project

---
+++ src/test/java/br/com/alura/admin/AdminApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.admin;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+class AdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
